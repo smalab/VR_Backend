26ad434c599b4917d26e80a825a8f96054e0f818
oitsmalab <oitsmalab@gmail.com>
2015-07-29 15:56:08 +0900

    7/29

---
+++ Photon/photon view tuto/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : Photon.MonoBehaviour {
+	
+	void Awake () {
+		PhotonNetwork.ConnectUsingSettings ("v0.1");
+	}
+
+	void Update () {
+	}
+
+	void OnJoinedLobby(){
+		PhotonNetwork.JoinRandomRoom ();
+	}
+
+	void OnPhotonRandomJoinFailed(){
+		Debug.Log ("ルームへの参加に失敗しました");
+		PhotonNetwork.CreateRoom (null);
+	}
+
+	void OnJoinedRoom(){
+		Debug.Log ("ルームへの参加に成功しました");
+		Vector3 spawnPosition = new Vector3(0,2,0);
+		PhotonNetwork.Instantiate ("PlayerPrefab", spawnPosition, Quaternion.identity, 0);
+	}
+
+	void OnGUI(){
+		GUILayout.Label (PhotonNetwork.connectionStateDetailed.ToString ());
+	}
+}
--- Photon/photon view tuto/Assets/Scripts/PlayerController.cs
+++ Photon/photon view tuto/Assets/Scripts/PlayerController.cs
@@ -2,22 +2,22 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-
+	
 	public float jumpPower = 300f;
 	public float movePower = 10f;
-
+	
 	void Start () {
 	}
-
-	void Update () {
 	
+	void Update () {
+		
 		float inputX = Input.GetAxis ("Horizontal");
 		float inputY = Input.GetAxis ("Vertical");
 		Vector2 force = new Vector2 (inputX, inputY) * movePower;
-		GetComponent<Rigidbody2D>().AddForce (force);
-
+		rigidbody2D.AddForce (force);
+		
 		if (Input.GetButtonDown ("Jump")) {
-			Rigidbody2D.AddForce (Vector2.up * jumpPower);
+			rigidbody2D.AddForce (Vector2.up * jumpPower);
 		}
 	}
 }
