7b5617e8d96f8847c18d1d87b220a9c7125c7968
oitsmalab <oitsmalab@gmail.com>
2015-09-11 12:30:36 +0900

    9/11 作業　プレイヤーの動きについていくカメラワーク機能を実装した。

---
+++ Photon/photon view tuto/Assets/Scripts/FollowingCameraScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowingCameraScript : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject target;
+	[SerializeField]
+	private Vector3 distance = new Vector3(0, 2, -4);
+	[SerializeField]
+	private Vector3 lookPoint = new Vector3(0, 1.35f, 0);
+
+	void Start () {
+
+	}
+
+	void Update () {
+		this.transform.position = target.transform.position + distance;
+		
+		Vector3 lookVector = target.transform.position + lookPoint - this.transform.position;
+		this.transform.rotation = Quaternion.LookRotation(lookVector);
+	}
+}
