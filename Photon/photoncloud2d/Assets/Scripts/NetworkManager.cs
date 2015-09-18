33b1fbc6d0f9b364b5a8b6c6393b3f6ece78736e
oitsmalab <oitsmalab@gmail.com>
2015-09-18 11:28:43 +0900

    9/18 サンプル製作のチュートリアル前編が終了した。

--- Photon/photoncloud2d/Assets/Scripts/NetworkManager.cs
+++ Photon/photoncloud2d/Assets/Scripts/NetworkManager.cs
@@ -1,15 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NetworkManager : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+public class NetworkManager : Photon.MonoBehaviour {
 	
+	void Awake(){
+		PhotonNetwork.ConnectUsingSettings ("v0.1");
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	void Update () {	
+	}
+
+	void OnJoinedLobby(){
+		PhotonNetwork.JoinRandomRoom ();
+	}
+
+	void OnPhotonRandomJoinFailed(){
+		Debug.Log ("ルームへの参加に失敗しました");
+		PhotonNetwork.CreateRoom (null);
+	}
+
+	void OnJoinedRoom(){
+		Debug.Log ("ルームへの参加に成功しました");
+	}
+
+	void OnGUI(){
+		GUILayout.Label (PhotonNetwork.connectionStateDetailed.ToString ());
 	}
 }
