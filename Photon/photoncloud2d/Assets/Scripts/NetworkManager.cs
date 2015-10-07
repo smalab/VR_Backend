c5093067ec3da2c60a0f087bc201ab7f274e5d5a
oitsmalab <oitsmalab@gmail.com>
2015-10-07 14:21:25 +0900

    10/7 ゲーム機として部屋に入った際に、プレイヤーを生成するようにした。

--- Photon/photoncloud2d/Assets/Scripts/NetworkManager.cs
+++ Photon/photoncloud2d/Assets/Scripts/NetworkManager.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class NetworkManager : Photon.MonoBehaviour {
 
 	public GameObject player;
+	public int flag = 0;
 	
 	void Awake(){
 		PhotonNetwork.ConnectUsingSettings ("v0.1");
@@ -24,8 +25,15 @@ public class NetworkManager : Photon.MonoBehaviour {
 	void OnJoinedRoom(){
 		Debug.Log ("ルームへの参加に成功しました");
 		Vector3 spawnPosition = new Vector3 (0, 2, 0);
-		var obj = PhotonNetwork.Instantiate ("PlayerPrefab", spawnPosition , Quaternion.identity, 0);
-		obj.name = "PlayerPrefab1";
+
+		if (flag == 0) {
+			var obj = PhotonNetwork.Instantiate ("PlayerPrefab", spawnPosition, Quaternion.identity, 0);
+			obj.name = "PlayerPrefab1";
+			Debug.Log ("ゲーム機です!!プレイヤーの生成に成功しました!");
+			flag = 1;
+		} else {
+			Debug.Log ("観客機です!!");
+		}
 	}
 
 	void OnGUI(){
