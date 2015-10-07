e2fe0aa813e62b9851d4978670cf68677d7a82b5
oitsmalab <oitsmalab@gmail.com>
2015-10-07 16:24:40 +0900

    10/7 Find関数を使ってゲームオブジェクトの検索をできるようにした。if文の条件がまだ完璧ではない。

--- Photon/photoncloud2d/Assets/Scripts/NetworkManager.cs
+++ Photon/photoncloud2d/Assets/Scripts/NetworkManager.cs
@@ -5,6 +5,7 @@ public class NetworkManager : Photon.MonoBehaviour {
 
 	public GameObject player;
 	public int flag = 0;
+	public GameObject str = null;
 	
 	void Awake(){
 		PhotonNetwork.ConnectUsingSettings ("v0.1");
@@ -25,6 +26,10 @@ public class NetworkManager : Photon.MonoBehaviour {
 	void OnJoinedRoom(){
 		Debug.Log ("ルームへの参加に成功しました");
 		Vector3 spawnPosition = new Vector3 (0, 2, 0);
+
+		if(str == GameObject.Find ("PlayerPrefab1")){
+			flag = 1;
+		}
 
 		if (flag == 0) {
 			var obj = PhotonNetwork.Instantiate ("PlayerPrefab", spawnPosition, Quaternion.identity, 0);
