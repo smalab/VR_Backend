27f26e4bb0989b04cbec987cca36e002a7b1c200
oitsmalab <oitsmalab@gmail.com>
2015-11-11 15:50:18 +0900

    11/11 色変更をする部分が見つけられなかったが、EditShaderを見つけたので変更できそう。

--- game_12_ShootingGame/12_ShootingGame/Assets/Scripts/WallColor.cs
+++ game_12_ShootingGame/12_ShootingGame/Assets/Scripts/WallColor.cs
@@ -2,15 +2,12 @@
 using System.Collections;
 
 public class WallColor : MonoBehaviour {
-
-	public int colorflag = 0;
-
+	
 	void Start () {
-	
+
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-	
+
 	}
 }
