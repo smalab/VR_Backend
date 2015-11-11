ecc3c22f422635f39007fd32314f1df55f46bfd3
oitsmalab <oitsmalab@gmail.com>
2015-11-11 15:01:04 +0900

    11/11 Planeを使って移動範囲の制限を可視化した壁を作成した。色が背景と同化して見にくい。

---
+++ game_12_ShootingGame/12_ShootingGame/Assets/Scripts/WallColor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallColor : MonoBehaviour {
+
+	public int colorflag = 0;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
